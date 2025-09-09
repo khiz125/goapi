8287226a5245c9a0d158581e8255f69bfa0b05d8
khiz125 <ken.hiro118@gmail.com>
2025-09-09 23:05:09 +0900

    refactor

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,123 +1,88 @@
 package handlers
 
 import (
-  "fmt"
-  "io"
-  "net/http"
-  "strconv"
-  "encoding/json"
-  "errors"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 
-  "github.com/gorilla/mux"
-  
-  "github.com/khiz125/goapi/mock"
-  "github.com/khiz125/goapi/domain"
+	"github.com/gorilla/mux"
+
+	"github.com/khiz125/goapi/domain"
+	"github.com/khiz125/goapi/mock"
 )
 
-
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-  io.WriteString(w, "Hello go world!\n")
+	io.WriteString(w, "Hello go world!\n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
-  length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-  if err != nil {
-    http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-    return
-  }
+	var reqArticle domain.Article
 
-  reqBodybuffer := make([]byte, length)
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
+	}
 
-  if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-    http.Error(w, "failed to get request body\n", http.StatusBadRequest)
-    return
-  }
+	article := reqArticle
 
-  defer req.Body.Close()
-
-  var reqArticle domain.Article
-
-  if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
-    http.Error(w, "failed to decode json\n", http.StatusBadRequest)
-    return
-  }
-
-  article := reqArticle
-  jsonData, err := json.Marshal(article)
-
-  if err != nil {
-    http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-    return
-  }
-
-  w.Write(jsonData)
+	json.NewEncoder(w).Encode(article)
 }
 
-
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-  queryMap := req.URL.Query()
-  var page int
-  if p, ok := queryMap["page"]; ok && len(p) > 0 {
-    var err error
-    page, err = strconv.Atoi(p[0])
-    if err != nil {
-      http.Error(w, "Invalid query parameter", http.StatusBadRequest)
-      return
-    }
-  } else {
-    page = 1
-  }
-  
-  articleList := []domain.Article{ mock.Article1, mock.Article2 }
-  jsonData, err := json.Marshal(articleList)
+	queryMap := req.URL.Query()
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+	log.Println(page) // FIX ME
 
-  if err != nil {
-    errMsg := fmt.Sprintf("failed to encode json (page %d)\n", page)
-    http.Error(w, errMsg, http.StatusInternalServerError)
-    return
-  }
-  
-  w.Write(jsonData)
+	articleList := []domain.Article{mock.Article1, mock.Article2}
+
+	json.NewEncoder(w).Encode(articleList)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-  articleID, err := strconv.Atoi(mux.Vars(req)["id"])
-  if err != nil {
-    http.Error(w, "Invalid query parameter", http.StatusBadRequest)
-    return
-  }
-  article := mock.Article1
-  jsonData, err := json.Marshal(article)
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 
-  if err != nil {
-    errMsg := fmt.Sprintf("failed to encode json (articleID %d)\n", articleID)
-    http.Error(w, errMsg, http.StatusInternalServerError)
-    return
-  }
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 
-  w.Write(jsonData)
+	log.Println(articleID) // FIX ME
+
+	article := mock.Article1
+
+	json.NewEncoder(w).Encode(article)
+
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-  article := mock.Article1
-  jsonData, err := json.Marshal(article)
+	var reqArticle domain.Article
 
-  if err != nil {
-    http.Error(w, "failed to encode json\n", http.StatusInternalServerError)
-    return
-  }
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
+	}
 
-  w.Write(jsonData)
+	article := reqArticle
+	json.NewEncoder(w).Encode(article)
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-  comment := mock.Comment1
-  jsonData, err := json.Marshal(comment)
-  if err != nil {
-    http.Error(w, "failed to encode json\n", http.StatusInternalServerError)
-    return
-  }
-  w.Write(jsonData)
-}
+	var reqComment domain.Comment
 
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
+	}
+	comment := reqComment
+	json.NewEncoder(w).Encode(comment)
+}
--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
   r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
   r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
   r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodGet)
-  r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodGet)
+  r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+  r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
   log.Println("Server start at port 8080")
   log.Fatal(http.ListenAndServe(":8080", r))
