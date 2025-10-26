d9094c3df21e7364d6b6b7179cc5e1bdab268ad3
khiz125 <ken.hiro118@gmail.com>
2025-10-26 17:05:33 +0900

    refactor, add controller, delete handler

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,4 +1,4 @@
-package handlers
+package controllers
 
 import (
 	"encoding/json"
@@ -12,29 +12,36 @@ import (
 	"github.com/khiz125/goapi/services"
 )
 
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
+type AppController struct {
+	service *services.AppService
+}
+
+func NewAppController(s *services.AppService) *AppController {
+	return &AppController{service: s}
+}
+
+func (c *AppController) HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello go world!\n")
 }
 
-func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-
+func (c *AppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle domain.Article
-
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
 	}
 
-	article, err := services.PostArticleService(reqArticle)
+	article, err := c.service.PostArticleService(reqArticle)
 	if err != nil {
-		http.Error(w, "failed to internal exec\n", http.StatusInternalServerError)
+		http.Error(w, "failed internal exec\n", http.StatusInternalServerError)
 		return
 	}
 
 	json.NewEncoder(w).Encode(article)
 }
 
-func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+func (c *AppController) ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
+
 	var page int
 	if p, ok := queryMap["page"]; ok && len(p) > 0 {
 		var err error
@@ -47,16 +54,16 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 		page = 1
 	}
 
-	articleList, err := services.GetArticleListService(page)
+	articleList, err := c.service.GetArticleListService(page)
 	if err != nil {
-		http.Error(w, "failed to internal exec\n", http.StatusInternalServerError)
+		http.Error(w, "failed internal exec\n", http.StatusInternalServerError)
 		return
 	}
 
 	json.NewEncoder(w).Encode(articleList)
 }
 
-func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+func (c *AppController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 
 	if err != nil {
@@ -64,7 +71,7 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	article, err := services.GetArticleService(articleID)
+	article, err := c.service.GetArticleService(articleID)
 	if err != nil {
 		http.Error(w, "failed to internal exex\n", http.StatusInternalServerError)
 		return
@@ -74,14 +81,14 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+func (c *AppController) PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle domain.Article
 
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
 	}
 
-	article, err := services.PostNiceService(reqArticle)
+	article, err := c.service.PostNiceService(reqArticle)
 	if err != nil {
 		http.Error(w, "failed to internal exec\n", http.StatusInternalServerError)
 		return
@@ -89,13 +96,13 @@ func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(article)
 }
 
-func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+func (c *AppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	var reqComment domain.Comment
 
 	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
 		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
 	}
-	comment, err := services.PostCommentService(reqComment)
+	comment, err := c.service.PostCommentService(reqComment)
 	if err != nil {
 		http.Error(w, "failed to internal exec\n", http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
-	"github.com/khiz125/goapi/handlers"
+	"github.com/khiz125/goapi/controllers"
+	"github.com/khiz125/goapi/services"
+)
+
+var (
+	dbUser     = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbDatabase = os.Getenv("DB_NAME")
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
 func main() {
 
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("failed to connect DB")
+		return
+	}
+
+	service := services.NewAppService(db)
+	controller := controllers.NewAppController(service)
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", controller.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", controller.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", controller.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", controller.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", controller.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", controller.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("Server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
